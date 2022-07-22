c49445889a5f45abbe31e5e6e79c3051b14d324f
hakimghaziali@gmail.com <hakimghaziali@gmail.com>
2022-07-22 14:14:52 +0430

    create detail view

--- books/models.py
+++ books/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 
@@ -15,3 +15,7 @@ class Book(models.Model):
     def __str__(self):
 
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("book_detail", args=[self.id] )
+    
